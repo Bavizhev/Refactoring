36e55ef3fbc4e8c86a3b6edd800f3800dc06a746
Bavizhev <ilyas4bavizhev@gmail.com>
2024-03-25 15:26:55 +0300

    Feature/Handler

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        final var server = new Server();
+
+        server.addHandler("GET", "/messages", (request, responseStream) -> {
+            // Handler code for GET /messages
+        });
+
+        server.addHandler("POST", "/messages", (request, responseStream) -> {
+            // Handler code for POST /messages
+        });
+
+        server.listen(9999);
     }
 }
